94894c3d66fb79dfade8ee28c06b4e0de79a6b64
krishna yadav <111423042+krishnaayadav@users.noreply.github.com>
2022-08-19 11:39:21 +0530

    Add files via upload

---
+++ InputVowel.java
@@ -0,0 +1,29 @@
+  /*
+PROGRAM: java program to check the input is vowel or constant
+@author: krishna yadav
+@date: 18 august 2022 
+*/    
+	  	class InputVowel
+	{
+	  public static void InputVowel()
+	  
+	{
+       char vowel = 'o';       //calling as character
+    int ascii = vowel;	       //integer taking
+           if(vowel == 'a' || vowel == 'e' || vowel == 'i' || vowel == 'o' || vowel == 'u')	
+			   System.out.println(vowel + "is vowel");
+		    else
+				System.out.println(vowel + "is consonant");
+		   
+		   if(ascii == 'a' || ascii == 'e' || vowel == 'i' || vowel == 'o' || vowel == 'u')
+			        System.out.println(ascii);
+	}
+	         public static void main(String argu[])
+			 
+			 {
+				 InputVowel();
+				 
+			 }
+			 
+	}
+	   
