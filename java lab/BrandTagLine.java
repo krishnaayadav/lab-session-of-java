97fdb3ffebe9befdf176c838537da563bdd83c08
krishna yadav <111423042+krishnaayadav@users.noreply.github.com>
2022-08-20 16:31:31 +0530

    Add files via upload

---
+++ java lab/BrandTagLine.java
@@ -0,0 +1,26 @@
+class BrandTagLine
+ { 
+    static void BrandName()
+	{ 
+	     String tagline ="Apple"; 
+		 switch (tagline)
+		 {
+			 
+			   case"Nike" : System.out.println("just do it");
+			      break;
+			  case"Puma" : System.out.println("FOREVER FASTER IS ALWAYS FIRST, NEVER SECOND, NEVER THIRD");
+                  break;
+              case"Adidas" : System.out.println("Adidas is all in");
+                  break;
+              case"Apple" : System.out.println("Think Different");
+                  break;
+              case"Boost" : System.out.println("Love Life");
+                  break;
+		 }
+    }
+     public static void main(String args[])
+	 { 
+	     BrandName();
+	 }
+	 
+ } 
